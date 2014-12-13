3beb91f8460649b1fa5d776094a8f32b6dc95629
MasuqaT <occar@hotmail.co.jp>
2014-12-14 00:13:03 +0900

    Solution initial commit

---
+++ LanguageExtAdornment/Factory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MasuqatNet.LanguageExtAdornment
+{
+	[Export(typeof(IWpfTextViewCreationListener))]
+	[ContentType("text")]
+	[TextViewRole(PredefinedTextViewRoles.Document)]
+	internal sealed class Factory : IWpfTextViewCreationListener
+	{
+		[Export(typeof(AdornmentLayerDefinition))]
+		[Name("LanguageExtAdornment")]
+		[Order(After = PredefinedAdornmentLayers.Selection, Before = PredefinedAdornmentLayers.Text)]
+		public AdornmentLayerDefinition editorAdornmentLayer = null;
+
+		public void TextViewCreated(IWpfTextView textView)
+		{
+		}
+	}
+}
